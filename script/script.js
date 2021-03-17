59a3813240e90f3562b11bf56a4af75edc58a26f
zzupd <zzupd@naver.com>
2021-03-17 16:06:18 +0900

    0317_GNB_작업중

--- script/script.js
+++ script/script.js
@@ -1,5 +1,17 @@
 $(function(){
 
 
+    /* GNB 서브메뉴 애니메이션 */
+    $("#gnb li").mouseover(function(){
+        $("#gnb ul.subMenu").stop().fadeIn(1000);
+        $("#subMenuBG").stop().fadeIn(1000);
+    });
+
+    $("#gnb li").mouseout(function(){
+        $("#gnb ul.subMenu").stop().fadeOut(600);
+        $("#subMenuBG").stop().fadeOut(600);
+    });
+    /* GNB 서브메뉴 애니메이션 */
+
 
 });
