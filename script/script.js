341c9106e53bef252df0a14941d02a7207e4f7a9
zzupd <zzupd@naver.com>
2021-03-17 16:49:02 +0900

    0317_GNB완료&Slideshow완료

--- script/script.js
+++ script/script.js
@@ -2,16 +2,31 @@ $(function(){
 
 
     /* GNB 서브메뉴 애니메이션 */
-    $("#gnb li").mouseover(function(){
+    $("#gnb a").mouseover(function(){
         $("#gnb ul.subMenu").stop().fadeIn(1000);
         $("#subMenuBG").stop().fadeIn(1000);
     });
 
-    $("#gnb li").mouseout(function(){
+    $("#gnb a").mouseout(function(){
         $("#gnb ul.subMenu").stop().fadeOut(600);
         $("#subMenuBG").stop().fadeOut(600);
     });
     /* GNB 서브메뉴 애니메이션 */
+
+    /* 슬라이드 쇼 */
+    setInterval(fnSlide, 3000);
+    function fnSlide() {
+
+        $("#slide a:first-child").fadeOut(500);
+        $("#slide a:nth-child(2)").fadeIn(
+            1000,
+            function() {
+                $("#slide a:first-child").insertAfter("#slide a:last-child");
+            }
+        );
+
+    }
+    /* 슬라이드 쇼 */
 
 
 });
